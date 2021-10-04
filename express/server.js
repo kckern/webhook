9fb24a14fa7b1444b37c1aa2f237aa719bce1f9c
KC Kern <kc@kckern.com>
2021-10-05 08:28:08 +0900

    Request Logging

--- express/server.js
+++ express/server.js
@@ -8,7 +8,10 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!!</h1>');
+  res.write('<h1>REQUEST:</h1>');
+  res.write('<pre>'+JSON.stringify(req)+'</pre>');
+  console.log("Request:");
+  console.log(req)
   res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
